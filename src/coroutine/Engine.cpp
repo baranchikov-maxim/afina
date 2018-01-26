2ff9c424942b293bb955ffd729a1684dba275bc6
max <baranchikov.maxim@gmail.com>
2018-01-26 13:18:39 +0300

    coroutine fix

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -3,84 +3,50 @@
 #include <setjmp.h>
 #include <stdio.h>
 #include <string.h>
-#include <algorithm>
 
 namespace Afina {
 namespace Coroutine {
 
 void Engine::Store(context &ctx) {
-  char StackEndsHere;
-  ctx.Low = std::min(&StackEndsHere, this->StackBottom);
-  ctx.High = std::max(&StackEndsHere, this->StackBottom);
-
-  ptrdiff_t l = ctx.High - ctx.Low;
-  if (l > std::get<1>(ctx.Stack)) {
-    delete[] std::get<0>(ctx.Stack);
-    std::get<0>(ctx.Stack) = new char[l];
-  }
-
-  std::get<1>(ctx.Stack) = l;
-  memcpy(std::get<0>(ctx.Stack), ctx.Low, l);
+	volatile char stack_end;
+	if (std::get<0>(ctx.Stack) != nullptr)
+		delete[] std::get<0>(ctx.Stack);
+	uint32_t len = StackBottom - &stack_end;
+	ctx.Stack = std::make_tuple(new char[len], len);
+	memcpy(std::get<0>(ctx.Stack), (const void*)&stack_end, len);
 }
 
 void Engine::Restore(context &ctx) {
-  char StackEndsHere;
-  if ((ctx.Low < &StackEndsHere) && (&StackEndsHere < ctx.High)) {
-    this->Restore(ctx);
-  }
+	volatile char stack_end;
+	if (&stack_end > StackBottom - std::get<1>(ctx.Stack))
+		Restore(ctx);
 
-  memcpy(ctx.Low, std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
-  longjmp(ctx.Environment, 1);
+	memcpy(StackBottom - std::get<1>(ctx.Stack),
+		std::get<0>(ctx.Stack),
+		std::get<1>(ctx.Stack));
+
+	longjmp(ctx.Environment, 1);
 }
 
-
-void Engine::yield(void) {
-  if (!this->alive) {
-    return;
-  }
-  context* target = this->alive;
-  if (this->alive == this->cur_routine) {
-    target = this->alive->next;
-  }
-  if (target->next != NULL) {
-    target->next->prev = target->prev;
-  }
-  if (target->prev != NULL) {
-    target->prev->next = target->next;
-  }
-  force_sched(target);
+void Engine::yield() {
+	if (alive != nullptr) {
+		context *ctx = alive;
+		alive = alive->next;
+		return sched(ctx);
+	}
 }
 
 void Engine::sched(void *routine_) {
-  context* ctx = static_cast<context*>(routine_);
-  if (ctx == this->cur_routine) {
-    return;
-  }
+	context *ctx = (context*)routine_;
 
-  context* ind = ctx;
-  while(ind != NULL) {
-    if (ind == this->cur_routine) {
-      if (ctx->caller != NULL) {
-        ctx->caller->callee = NULL;
-      }
+	if (cur_routine != nullptr) {
+		Store(*cur_routine);
+		 if (setjmp(cur_routine->Environment) > 0) {
+            return;
+        }
     }
-    ind = ind->caller;
-  }
-
-  force_sched(ctx);
-}
-
-void Engine::force_sched(void* routine_) {
-  context* ctx = static_cast<context*>(routine_);
-  if (this->cur_routine != NULL) {
-    cur_routine->callee = ctx;
-    Store(*cur_routine);
-    if (setjmp(cur_routine->Environment) != 0) {
-      return;
-    }
-  }
-  this->cur_routine = ctx;
-  this->Restore(*ctx);
+    cur_routine = ctx;
+    Restore(*ctx);
 }
 
 } // namespace Coroutine
